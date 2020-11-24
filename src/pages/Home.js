01fec4c36085281887e778736440d2754f4719de
Saul Montes De Oca <mr.sauce97@gmail.com>
2020-11-24 12:34:55 -0600

    Added button

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '../components/Button';
+
+const Home = () => {
+    return (
+        <div>
+            <Button />
+        </div>
+    );
+};
+
+export default Home;
