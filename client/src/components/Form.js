ed81e62cd1e39382a36456f08a0af3adaf308eb6
Shivay Lamba <shivaylamba@gmail.com>
2020-11-25 20:14:14 +0530

    download form file

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,42 +1,47 @@
-import React from 'react';
-import { Form, Row, Col } from 'react-bootstrap';
-const Formm = () => {
-    return (
-        <div>
-            <Form>
-                <Row className="justify-content-around">
-                    <Col lg={2}>
-                        <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Label>Select frontend</Form.Label>
-                            <Form.Control as="select">
-                            <option>React</option>
-                            <option>Angular</option>
-                            <option>Vue</option>
-                            </Form.Control>
-                        </Form.Group>
-                    </Col>
-                    <Col lg={2}>
-                        <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Label>Select backend</Form.Label>
-                            <Form.Control as="select">
-                            <option>Django</option>
-                            <option>Node</option>
-                            </Form.Control>
-                        </Form.Group>
-                    </Col>
-                    <Col lg={2}>
-                        <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Label>Select package manager</Form.Label>
-                            <Form.Control as="select">
-                            <option>npm</option>
-                            <option>yarn</option>
-                            </Form.Control>
-                        </Form.Group>
-                    </Col>
-                </Row>
-            </Form>
-        </div>
-    );
+import React from "react";
+import { Form, Row, Col } from "react-bootstrap";
+
+const Formm = ({ formData, handleChange }) => {
+	return (
+		<div>
+			<Form>
+				<Row className="justify-content-around">
+					<Col lg={2}>
+						<Form.Group controlId="exampleForm.ControlSelect1">
+							<Form.Label>Select frontend</Form.Label>
+							<Form.Control name="frontend" as="select" onChange={handleChange}>
+								<option>React</option>
+								<option>Angular</option>
+								<option>Vue</option>
+							</Form.Control>
+						</Form.Group>
+					</Col>
+					<Col lg={2}>
+						<Form.Group controlId="exampleForm.ControlSelect1">
+							<Form.Label>Select backend</Form.Label>
+							<Form.Control name="backend" onChange={handleChange} as="select">
+								<option>Django</option>
+								<option>Node</option>
+							</Form.Control>
+						</Form.Group>
+					</Col>
+					<Col lg={2}>
+						<Form.Group
+							name="packageManager"
+							onChange={handleChange}
+							controlId="exampleForm.ControlSelect1"
+						>
+							<Form.Label>Select package manager</Form.Label>
+							<Form.Control as="select">
+								<option>npm</option>
+								<option>yarn</option>
+							</Form.Control>
+						</Form.Group>
+					</Col>
+				</Row>
+			</Form>
+		</div>
+	);
 };
 
 export default Formm;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import { Button, Image, Jumbotron } from 'react-bootstrap';
-import NavBar from '../components/Navbar';
-import Form from '../components/Form';
-import logo from '../assets/bg.jpg'
+import React, { useState } from "react";
+import { Button, Image, Jumbotron } from "react-bootstrap";
+import NavBar from "../components/Navbar";
+import Form from "../components/Form";
 
 const Home = () => {
-    return (
-        <>
-            <NavBar />
-            <Jumbotron>
-                <h1>Welcome to MyBoilerPlate!</h1>
-                <p>
-                    You can build your own boiler-plate 
-                    for any project you want.
-                </p>
-                <p>
-                    <Button variant="primary">Start</Button>
-                </p>
-            </Jumbotron>
-            <Form />
-            <Button variant="outline-success">Download my BoilerPlate!</Button>
-        </>
-    );
+	const [formData, setFormData] = useState({
+		frontend: "Angular",
+		backend: "Node",
+		packageManager: "npm",
+	});
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+	return (
+		<>
+			<NavBar />
+			<Jumbotron>
+				<h1>Welcome to MyBoilerPlate!</h1>
+				<p>You can build your own boiler-plate for any project you want.</p>
+				<p>
+					<Button variant="primary">Start</Button>
+				</p>
+			</Jumbotron>
+			<Form handleChange={handleChange} formData={formData} />
+			<a
+				href={`/${formData.frontend}_${formData.backend}_${formData.packageManager}.zip`} // File Name: "Angular_Node_npm.zip"
+				variant="outline-success"
+				download
+			>
+				Download my BoilerPlate!
+			</a>
+		</>
+	);
 };
 
 export default Home;
