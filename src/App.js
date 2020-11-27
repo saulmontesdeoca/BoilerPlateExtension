10214dadaf242f5d53c61ad23b5c98f1b7f20212
Saul Montes De Oca <mr.sauce97@gmail.com>
2020-11-27 15:23:12 -0600

    Adding Node and readme snippets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Home from './pages/Home';
 import Helpers from './pages/Helpers';
 import HelpersAngular from './pages/HelpersAngular';
 import HelpersVue from './pages/HelpersVue';
+import HelpersReadme from './pages/HelpersReadme';
+import HelpersNode from './pages/HelpersNode';
+
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -12,7 +15,8 @@ function App() {
               <Route path='/helpers/react' exact component={Helpers}/>
               <Route path='/helpers/angular' exact component={HelpersAngular}/>
               <Route path='/helpers/vue' exact component={HelpersVue}/>
-
+              <Route path='/helpers/readme' exact component={HelpersReadme}/>
+              <Route path='/helpers/node' exact component={HelpersNode}/>
             </Switch>
         </BrowserRouter>
   );
